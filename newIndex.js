c43cd6236758e7fe3947243ea61fadff1578a3ea
chinmoybuzz <chinmoybuzz@gmail.com>
2025-07-27 21:43:48 +0530

    newIndex, chatHanlder and auth changes

--- newIndex.js
+++ newIndex.js
@@ -5,38 +5,38 @@ const cors = require("cors");
 const apiRoute = require("./routes/api.v1.route");
 const { initialize } = require("./config/socket");
 // const { default: helmet } = require("helmet");
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 
 async function start() {
   try {
     const app = express();
     app.use(cors());
     // app.use(helmet())
-//     app.use(
-//   helmet({
-//     contentSecurityPolicy: {
-//       directives: {
-//         defaultSrc: ["'self'"],
-//         scriptSrc: [
-//           "'self'",
-//           "'unsafe-inline'", // Optional: Needed if you're using inline scripts or Vue/React dev tools
-//           "https://cdn.socket.io", // Allow Socket.IO if served from CDN
-//           "http://localhost:3000", // Adjust based on your backend
-//         ],
-//         connectSrc: [
-//           "'self'",
-//           "ws://localhost:3000", // For WebSocket connection
-//           "http://localhost:3000",
-//         ],
-//         styleSrc: ["'self'", "'unsafe-inline'"],
-//         imgSrc: ["'self'", "data:"],
-//         fontSrc: ["'self'"],
-//         objectSrc: ["'none'"],
-//         upgradeInsecureRequests: [],
-//       },
-//     },
-//   })
-// );
+    //     app.use(
+    //   helmet({
+    //     contentSecurityPolicy: {
+    //       directives: {
+    //         defaultSrc: ["'self'"],
+    //         scriptSrc: [
+    //           "'self'",
+    //           "'unsafe-inline'", // Optional: Needed if you're using inline scripts or Vue/React dev tools
+    //           "https://cdn.socket.io", // Allow Socket.IO if served from CDN
+    //           "http://localhost:3000", // Adjust based on your backend
+    //         ],
+    //         connectSrc: [
+    //           "'self'",
+    //           "ws://localhost:3000", // For WebSocket connection
+    //           "http://localhost:3000",
+    //         ],
+    //         styleSrc: ["'self'", "'unsafe-inline'"],
+    //         imgSrc: ["'self'", "data:"],
+    //         fontSrc: ["'self'"],
+    //         objectSrc: ["'none'"],
+    //         upgradeInsecureRequests: [],
+    //       },
+    //     },
+    //   })
+    // );
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.static("public"));
@@ -53,8 +53,9 @@ async function start() {
       process.exit(1); // Optional: force exit on startup failure
     });
   } catch (error) {
-  console.error(`Startup error: ${error.message}`);
-    process.exit(1);  }
+    console.error(`Startup error: ${error.message}`);
+    process.exit(1);
+  }
 }
 
 // Global error handlers (best practice)
